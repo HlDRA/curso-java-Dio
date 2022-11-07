2ad0e889732764e13927379417a23e69834322c8
Pedro Henrique da Silva <pedro.hsilva.pe@gmail.com>
2022-11-06 22:30:16 -0300

    Criação de três métodos
    Criando os métodos para tirar media, fazer a busca em um vetor de formar linear e criando o método de ordenação InsertionSort.

--- Primeiro projeto java DIO/src/App.java
+++ Primeiro projeto java DIO/src/App.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
+
 public class App {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hel, World!");
+        int[] vetorX = {5,4,8,4,13,544,999,115,99,9,10,1,-54,112,0,0,1195,-45648};
+        insertionSort(vetorX);
+        for (int i = 0; i < vetorX.length; i++){
+            System.out.print(vetorX[i] + " ");
+        }
+        
+    }
+
+
+    public static double tirarMedia(){
+        double resultado = 0;
+        double somatorio = 0;
+        System.out.println("Digite a quantidade de numeros que deseja tirar a media: ");
+        int quantidadeNumeros = sc.nextInt();
+        for (int i = 0; i < quantidadeNumeros; i++){
+            System.out.println("escolha o numero na posiÃ§Ã£o "+ (i+1) + ": ");
+            somatorio += sc.nextDouble();
+        }
+        resultado = somatorio/quantidadeNumeros;
+        return resultado;
+    }
+
+
+    public static int buscaLinear(int[] vetor, int chave){
+        int resultado = -1;
+        for (int i = 0; i < vetor.length; i++){
+            if (vetor[i] == chave) return i;
+        }
+        return resultado;
+    }
+
+
+    public static void insertionSort(int[] vetor){
+        int backup = 0;
+        int contador = 1;
+        do{
+            backup = vetor[contador];
+            for (int i = contador - 1; i >= 0; i--){
+                if (backup < vetor[i]){
+                    vetor[i + 1] = vetor[i];
+                    vetor[i] = backup;
+                }
+                else if (backup >= vetor[i]) break;
+            }
+            contador++;
+        }while (contador < vetor.length);
     }
 }
